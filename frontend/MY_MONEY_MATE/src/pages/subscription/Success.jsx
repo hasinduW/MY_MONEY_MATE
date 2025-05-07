157dfe3cc16e496d5f6883fe0eba911f69b3dba9
It22902184 <127286216+It22902184@users.noreply.github.com>
2025-05-07 15:15:06 +0530

    Adding payment function

--- frontend/MY_MONEY_MATE/src/pages/subscription/Success.jsx
+++ frontend/MY_MONEY_MATE/src/pages/subscription/Success.jsx
@@ -1,17 +1,19 @@
 import React from 'react';
-import '../../styles/PaymentSuccess.css'; // We'll create this for styling
+import '../../styles/PaymentSuccess.css';
 
 const PaymentSuccess = () => {
   return (
     <div className="payment-success-container">
       <div className="payment-success-card">
-        <div className="payment-success-icon">
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="green" width="48px" height="48px">
-            <path d="M0 0h24v24H0z" fill="none"/>
-            <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-2 15l-5-5 1.41-1.41L10 14.17l7.59-7.59L19 8l-9 9z"/>
-          </svg>
+        <div className="payment-success-header">
+          <div className="payment-success-icon-wrapper">
+            <svg className="payment-success-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="green">
+              <path d="M0 0h24v24H0z" fill="none"/>
+              <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-2 15l-5-5 1.41-1.41L10 14.17l7.59-7.59L19 8l-9 9z"/>
+            </svg>
+          </div>
+          <h1 className="payment-success-title">Payment Successful!</h1>
         </div>
-        <h1 className="payment-success-title">Payment Successful!</h1>
         <p className="payment-success-message">
           Thank you for your subscription. You now have access<br />
           to all the amazing features!
